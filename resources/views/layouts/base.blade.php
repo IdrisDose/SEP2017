a1fa5d5602c37c2afec91dfb71c91164ccc4fed8
IdrisDose <idrisdev@gmail.com>
2017-10-07 14:53:55 +1100

    Added Javascript reactive footer, so it's not so funky, added new BG Image.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -10,9 +10,16 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
-
-<body class="clearfix bg-inc">
-
+<noscript>
+    This site requires Javascript. Please enable Javascript in your browser for this site.<br/>
+    
+    Something make look broken without it, please enable it.
+</noscript>
+@if(Route::is('index') || Route::is('dashboard') || Route::is('admin') || Route::is('login') || Route::is('register'))
+    <body class="clearfix bg-inc">
+@else
+    <body class="clearfix bg-inc2">
+@endif
     <div class="container-fluid">
         <div class="row">
             @include('inc.navbar')
