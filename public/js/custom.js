d2fa4e69fc617aed168b82b71c83d82e1ce60a27
IdrisDose <idrisdev@gmail.com>
2017-10-09 14:20:37 +0000

    Attempted scroll bar fix

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
     // Check if body height is higher than window height :)
 
-    if (($("body").height() > $(window).height())) {
+    if (($("body").hasScrollBar())) {
         $("footer").addClass("static");
     }
 });
+
+(function($) {
+    $.fn.hasScrollBar = function() {
+        return this.get(0).scrollHeight > this.height();
+    }
+})(jQuery);
